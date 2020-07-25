95f9916f3fa5d6d8ac7d8f59633d3b2574959fd9
Aveline Wingfield <avelinewingfield@gmail.com>
2020-07-25 12:24:09 -0400

    SplashPage

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Front-End
+To Do:
+
+Saturday:
+Home Page -
+Large Image(placeholder) w/Fancy Bordered Sign Up Button
+NavBar - Home / Login
+No Header
+Drawer Style
+
+Routing!
+
+Sunday:
+SignUp Form - Centered - Loading Animation(placeholder)
+LogIn Form - Centered - Loading Animation(placeholder)
+NewUser Welcome Form (display anew)
+
+
+Stretch:
+Custom Loading Image: Filling Water Drop? Spinning Flower?
--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
 /* 
 Style Here:
 Background Image = Giant Fancy Button
@@ -8,23 +11,20 @@ Import the NavBar
 Check SignUp Route
 */
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <CssBaseline />
+      <Container style = {{maxWidth:"md"}}>
+        <Typography component="div" style={{display: 'flex', justifyContent: 'center', backgroundColor: '#cfe8fc', height: '100vh', opacity: '80%' }} >
+        <Button style = {{color:"#210124", maxHeight: '2.5rem', alignSelf: 'center' }}>
+          Sign Up
+        </Button>
+        </Typography>
+      </Container>
+      
     </div>
   );
 }
