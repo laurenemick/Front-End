3ea04086d4ba671d0cd17d02e01101ca927b3113
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-27 15:48:53 -0400

    Add BrowserRouter

--- water-my-plants/src/index.js
+++ water-my-plants/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'fontsource-roboto'; // material-ui default font
 
+// routes should always be contained inside a router
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
