287b7f8fc89625f7a6a4020c7f03047f3f74bb06
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-27 21:13:52 -0400

    Add styles courtesy of Ava

--- water-my-plants/src/components/SignUp.jsx
+++ water-my-plants/src/components/SignUp.jsx
@@ -1,4 +1,32 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+// courtesy of style master Ava
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center'
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "25ch"
+  },
+  cardroot: {
+    minWidth: 275,
+  },
+
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+}));
+
 
 // Sign up page
 export default function SignUp (props) {
