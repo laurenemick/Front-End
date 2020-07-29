40f7196d174e7ad87e9fc962330755aecae7eba2
Lauren Emick <laurenemick6@gmail.com>
2020-07-29 07:38:28 -0700

    structure w/o working endpoints

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
+import Dashboard from './components/Dashboard';
+// import Login from './components/Login';
+// import SignUp from './components/SignUp';
+// import About from './components/About';
+// import MarketingPage from './MarketingPage';
+
+// Utils
+// import { PrivateRoute } from './utils/PrivateRoute';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          {/* <Route exact path='/login' component={Login} /> */}
+          {/* <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/home' component={MarketingPage} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 export default App;
---
+++ water-my-plants/src/data.js
@@ -0,0 +1,30 @@
+export default [
+	{
+		id: 1,
+		nickname: 'nickname 1',
+        species: 'species 1',
+        h2oFrequency:'once a week',
+        image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		id: 2,
+		nickname: 'nickname 2',
+        species: 'species 2',
+        h2oFrequency:'once a week',
+        image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		id: 3,
+		nickname: 'nickname 3',
+        species: 'species 3',
+        h2oFrequency:'once a week',
+        image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		id: 4,
+		nickname: 'nickname 4',
+        species: 'species 4',
+        h2oFrequency:'once a week',
+        image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+	}
+];
