b63b2d205c83debf005efe54cfba009f9127c6fd
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-30 14:45:03 -0400

    loginFormSchema.js

--- water-my-plants/src/verification/loginFormSchema.js
+++ water-my-plants/src/verification/loginFormSchema.js
@@ -3,12 +3,11 @@ import * as yup from "yup";
 const LFormSchema = yup.object().shape({
   username: yup
     .string()
-    .max(320, "Username is too long")
     .required("Username is required"),
 
   password: yup
     .string()
-    .max(30, "Your password is too long")
+    .max(320, "Your password is too long")
     .required("Password is required"),
 });
 
